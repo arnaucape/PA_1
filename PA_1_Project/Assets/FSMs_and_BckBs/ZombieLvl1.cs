81b667231a136e0f7dff1d2b792436edb78d18c7
Oriol Ferrer <ktfk94@gmail.com>
2020-02-25 12:58:24 +0100

    Creat el 1r nivell de comportament

---
+++ PA_1_Project/Assets/FSMs_and_BckBs/ZombieLvl1.cs
@@ -0,0 +1,94 @@
+﻿using FSM;
+using Steerings;
+using UnityEngine;
+
+[RequireComponent(typeof(WanderPlusAvoid))]
+[RequireComponent(typeof(FlockingAroundPlusAvoid))]
+[RequireComponent(typeof(Zombie_Blackboard))]
+public class ZombieLvl1 : FiniteStateMachine
+{
+    public enum State { INITIAL, WANDER, FLOCK }; //Flock -> all zombies going to the sound act as a flock
+    public State currentState = State.INITIAL;
+
+    private WanderPlusAvoid wander;
+    private FlockingAroundPlusAvoid flock;
+    private Flee flee;
+    private KinematicState ks;
+    private GameObject bbObject;
+    private Zombie_Blackboard bbZombie;
+    private DynamicZombie_Blackboard bbInfo;
+
+    private GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wander = GetComponent<WanderPlusAvoid>();
+        flock = GetComponent<FlockingAroundPlusAvoid>();
+        ks = GetComponent<KinematicState>();
+        bbZombie = GetComponent<Zombie_Blackboard>();
+        bbInfo = GameObject.Find("DynamicBB").GetComponent<DynamicZombie_Blackboard>();
+
+        flock.enabled = false;
+        wander.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case State.INITIAL:
+                ChangeState(State.WANDER);
+                break;
+            case State.WANDER:
+                target = SensingUtils.FindInstanceWithinRadius(this.gameObject, bbZombie.soundTag, bbZombie.soundDetectableRadius);
+                if (target || (bbInfo.soundDetected && SensingUtils.DistanceToTarget(gameObject, bbInfo.soundDetected) <= bbZombie.soundTalkableRadius))
+                {
+                    ChangeState(State.FLOCK);
+                    break;
+                }
+                break;
+            case State.FLOCK:
+                if (!bbInfo.soundDetected)
+                {
+                    ChangeState(State.WANDER);
+                    break;
+                }
+                break;
+        }
+    }
+
+    void ChangeState(State newState)
+    {
+        switch (currentState)
+        {
+            case State.INITIAL: break;
+            case State.WANDER:
+                wander.enabled = false;
+                break;
+            case State.FLOCK:
+                flock.enabled = false;
+                target = null;
+
+                flock.attractor = null;                
+                break;
+        }
+
+        switch (newState)
+        {
+            case State.INITIAL: break;
+            case State.WANDER:
+                wander.enabled = true;
+                break;
+            case State.FLOCK:
+                if (target) bbInfo.soundDetected = target;
+                else target = bbInfo.soundDetected;
+
+                flock.attractor = target;
+                flock.enabled = true;
+                break;
+        }
+        currentState = newState;
+    }
+}
---
+++ PA_1_Project/Assets/FSMs_and_BckBs/Zombie_Blackboard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie_Blackboard : MonoBehaviour
+{
+    public float soundDetectableRadius = 60f;
+    public float soundTalkableRadius = 120f; //radius where others zombies can recieve the info of a sound, by another zombie
+
+    public float playerDetectioneRadius = 80f;
+    public float playerFarEnoughRadius = 150f;
+
+    public string zombieTag = "Zombie";
+    public string soundTag = "Sound";
+}
